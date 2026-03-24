a957a0f5ce9f1017a1bd5a5e155bc1b4131034a0
Sofia <sofgajardo6@gmail.com>
2026-03-24 20:07:23 -0300

    idk

--- src/pages/checklists/checklistARO.jsx
+++ src/pages/checklists/checklistARO.jsx
@@ -97,7 +97,7 @@ return (
     </div>
 
     <div style={{ overflowX: 'auto' }}>
-      <table style={{ width: '100%', borderCollapse: 'collapse', fontSize: '0.85rem' }}>
+      <table style={{ width: '100%', borderCollapse: 'collapse', fontSize: '0.85rem', tableLayout: 'fixed' }}>
         <thead>
           <tr style={{ background: '#c2185b', color: 'white' }}>
             <th style={{ padding: '10px', textAlign: 'left', minWidth: '250px' }}>Procedimiento</th>
