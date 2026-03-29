7438ef2e38a0329326d13a5986d0087fed986338
Sofia <sofgajardo6@gmail.com>
2026-03-29 01:34:08 -0300

    idk

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,23 @@ import { VitePWA } from 'vite-plugin-pwa'
 export default defineConfig({
   plugins: [
     react(),
-    VitePWA({ registerType: 'autoUpdate' })
+    VitePWA({
+  registerType: 'autoUpdate',
+  workbox: {
+    globPatterns: ['**/*.{js,css,html,png,jpg}']
+  },
+  manifest: {
+    name: 'MatronaApp',
+    short_name: 'MatronaApp',
+    icons: [
+      {
+        src: '/favicon.png',
+        sizes: '192x192',
+        type: 'image/png'
+      }
+    ]
+  }
+})
   ],
   base: '/'
 })
