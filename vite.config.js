9dc310ab7e6d0d89179eb37aea07c8ac59bb7394
Sofia <sofgajardo6@gmail.com>
2026-03-29 01:26:14 -0300

    app offline

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({ registerType: 'autoUpdate' })
+  ],
   base: '/'
 })
